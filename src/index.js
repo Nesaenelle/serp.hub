606dc183d42c9844d073d0607dcf9024f83d41b8
Bogdan Zinkevych <venomsunset@gmail.com>
2019-06-15 15:10:41 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './scss/style.scss'
 import $ from 'jquery';
 window.$ = window.jQuery = $;
 import 'slick-carousel';
+import './lightslider';
+
 
 $('[data-dropdown-value]').on('click', function(e) {
     e.stopPropagation();
@@ -44,6 +46,30 @@ $('.bottom-form form').on('submit', (e)=> {
   openModal('thanks');
 });
 
+$('.feedback__form').on('submit', (e)=> {
+  e.preventDefault();
+  let clone = $('.feedback__list__item').eq(0).clone();
+  let name = e.target.name.value;
+  let text = e.target.text.value;
+  let rating = e.target.rating.value;
+  clone.find('.feedback__list__item--name').text(name);
+  clone.find('.feedback__list__item--date').text(new Date().toLocaleString());
+  clone.find('.feedback__list__item--text').text(text);
+  $('.feedback__list').prepend(clone);
+
+  e.target.reset();
+});
+
+$('.rating img').on('click', function() {
+  var input = $(this).siblings('input');
+  var id =  $(this).data('id');
+  $(this).parent().find('img').attr('src', 'img/star.svg');
+
+  $(this).prevAll().attr('src', 'img/stargreen.svg');
+  $(this).attr('src', 'img/stargreen.svg');
+  input.val(id);
+});
+
 function openModal(id) {
   $('.modal').removeClass('active');
 
@@ -60,4 +86,46 @@ $('[data-modal-button]').on('click', function(e) {
 $('[data-modal-close]').on('click', function() {
     $('.modal--overlay').removeClass('active');
     $('.modal').removeClass('active');
+});
+
+
+var slider = $('#imageGallery').lightSlider({
+    gallery:true,
+    item:1,
+    loop:true,
+    thumbItem:4,
+    slideMargin: 0,
+    enableDrag: false,
+    // vThumbWidth:50,
+    thumbMargin: 12,
+    // verticalHeight:295,
+    currentPagerPosition:'left',
+    // responsive : [
+    //       {
+    //           breakpoint:800,
+    //           settings: {
+    //               item:3,
+    //               slideMove:1,
+    //               slideMargin:6,
+    //             }
+    //       },
+    //       {
+    //           breakpoint:480,
+    //           settings: {
+    //               item:2,
+    //               slideMove:1
+    //             }
+    //       }
+    //   ],
+    onSliderLoad: function(el) {
+
+    }   
+});  
+
+$('.prev-arrow').on('click', ()=> {
+  slider.goToPrevSlide(); 
+});
+
+$('.next-arrow').on('click', ()=> {
+  slider.goToNextSlide(); 
 });
